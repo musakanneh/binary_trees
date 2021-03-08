f6861986d172b03d46e2b15389ad0afcc638b5d8
musakanneh <mkanneh17@alustudent.com>
2021-03-08 21:40:52 +0200

    First commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_tress.h"
+
+/**
+ * binary_tree_node - a function that creates a binary node
+ * @parent: a pointer to the parent node of the node to create
+ * @value: the value to put in the new node
+ *
+ * Return: returs a pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+	return (0);
+}
