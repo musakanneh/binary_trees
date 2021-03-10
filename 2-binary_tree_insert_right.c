08295de1e3a9955b4be224b5abde87ccf5e1f423
musakanneh <mkanneh17@alustudent.com>
2021-03-10 14:57:41 +0200

    More attempts

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -14,9 +14,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent)
+	{
+		return (NULL);
+	}
+
 	new_node = malloc(sizeof(binary_tree_t));
 
-	if (!parent)
+	if (!new_node)
 	{
 		return (NULL);
 	}
