87d4658fc981d74ed0433502ade77d674e49a76e
musakanneh <mkanneh17@alustudent.com>
2021-03-10 14:52:28 +0200

    More attempts

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -34,6 +34,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
+	else
+	{
+		new_node->left = NULL;
+	}
 	parent->left = new_node;
 	return (new_node);
 }
