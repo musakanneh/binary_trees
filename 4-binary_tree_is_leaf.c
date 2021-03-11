7098c751dd2976279a36550dd6407088f571773b
musakanneh <mkanneh17@alustudent.com>
2021-03-11 21:59:11 +0200

    Redid test four

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,56 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - a functions that measures the
- * balance factor of a binary tree
- * @tree: pointer to root node of tree to measure
+ * binary_tree_is_leaf - Checks if a node
+ * is a leaf of a binary tree.
+ * @node: A pointer to the node to check.
  *
- * Return: balance factor left height minus right height,
- * or 0 if tree is NULL
+ * Return: if the node is a leaf - 1
+ * otherwise - 0.
  */
-int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	int left, right;
-
-	if (tree == NULL)
+	if (node == NULL || node->left != NULL || node->right != NULL)
 	{
 		return (0);
 	}
-	left = custom_binary_tree_height(tree->left);
-	right = custom_binary_tree_height(tree->right);
-	return (left - right);
-}
-
-/**
- * custom_binary_tree_height - Measure the height
- * of a binary tree from a given node
- * @tree: pointer to node of tree to measure
- *
- * Description: Edited to work with balance factor function
- *
- * Return: height of tree or -1 if NULL
- */
-int custom_binary_tree_height(const binary_tree_t *tree)
-{
-	int left, right;
-
-	if (tree == NULL)
-	{
-		return (-1);
-	}
-	if ((tree->left == NULL) && (tree->right == NULL))
-	{
-		return (0);
-	}
-	left = custom_binary_tree_height(tree->left) + 1;
-	right = custom_binary_tree_height(tree->right) + 1;
-
-	if (left > right)
-	{
-		return (left);
-	}
-	else
-	{
-		return (right);
-	}
+	return (1);
 }
